5d0a52223a04858437a944128bbac505c7a0e6ca
nemihail <nemihail02@gmail.com>
2026-01-27 21:52:23 +0700

    base operations

--- code.py
+++ code.py
@@ -23,3 +23,51 @@ def counter(func):
     wrapper.count = 0
     return wrapper
 
+
+@decorator
+@counter
+def add_product():
+    try:
+        name = input('Product name to add: ')
+        if name in product_list.keys():
+            command = input(f'\nProduct named "{name}" already exists.\nDo you want to update its quantity ({product_list[name]})? [y/n]: ')
+            if command == 'y':
+                pass
+            elif command == 'n':
+                return 0
+            else:
+                print('Input is incorrect')
+        product_list.update({
+            name:
+            int(input('Product quantity: '))
+        })
+    except TypeError:
+        print('Suka ti tupoi blyat')
+
+
+@decorator
+@counter
+def remove_product():
+    name = input('Product name to remove: ')
+    if name in product_list.keys():
+        del product_list[name]
+    else:
+        print('There is nothing to remove')
+
+
+@decorator
+@counter
+def replace_product():
+    try:
+        name = input('Product name to replace: ')
+        if name in product_list.keys():
+            del product_list[name]
+        else:
+            print('There is nothing to replace')
+        product_list.update({
+            name:
+            int(input('Product quantity: '))
+        })
+    except TypeError:
+        print('Suka ti tupoi blyat')
+
